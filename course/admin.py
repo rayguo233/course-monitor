d1188e4597352612362b7ab3d0793ca531b06242
Ray Guo <rayguo@Rays-Mac.local>
2020-09-18 00:32:38 +0800

    s

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Subject, Course
+from .models import Subject, Course, Email
 
 admin.site.register(Subject)
 admin.site.register(Course)
+admin.site.register(Email)
--- course/views.py
+++ course/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Course
-# Create your views here.
+from .forms import EmailCreationForm
+from .models import Email, Course
+
 def course_detail_view(request):
 	c = Course.objects.all()
 	context = {
 		'courses': c
 	}
-	return render(request, "course/detail.html", context)
+	return render(request, "course_detail.html", context)
+
+def course_add_view(request):
+	form = EmailCreationForm()
+	if request.method == 'POST':
+		form = EmailCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('course_add')
+	return render(request, 'course_home.html', {'form': form})
+
+def course_update_view(request):
+	email = get_object_or_404(Email, pk=pk)
+	form = EmailCreationForm(instance=email)
+	if request.method == 'POST':
+		form = EmailCreationForm(request.POST, instance=email)
+		if form.is_valid():
+			form.save()
+			return redirect('email_change', pk=pk)
+	return render(request, 'course_home.html', {'form': form})
+
+def load_courses(request):
+	subject_id = request.GET.get('subject_id')
+	courses = Course.objects.filter(subject_id=subject_id)
+	return render(request, 'course_dropdown_list_options.html', {'courses': courses})
+	# return JsonResponse(list(cities.values('id', 'name')), safe=False)
