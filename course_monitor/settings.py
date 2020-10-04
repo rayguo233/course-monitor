0b1f4dc28d7e34ede160f542f0ec5374d48bb2b7
Ray Guo <rayguo@vpn-172-27-3-245.host.ucla.edu>
2020-10-04 22:50:02 +0100

    add untrack html

--- course_monitor/settings.py
+++ course_monitor/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path # os is probably used in older version of django
-import django_heroku
+from pathlib import Path  # os is probably used in older version of django
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,14 +78,20 @@ WSGI_APPLICATION = 'course_monitor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'rayguo',
+#         'PASSWORD': os.environ.get('RDS_PASSWORD'),
+#         'HOST': os.environ.get('HOST'),
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'rayguo',
-        'PASSWORD': os.environ.get('RDS_PASSWORD'),
-        'HOST': os.environ.get('HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
@@ -135,6 +140,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'course_add'
 
 LOGIN_URL = 'login'
-
-# Activate Django-Heroku.
-# django_heroku.settings(locals())
