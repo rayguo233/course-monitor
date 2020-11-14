a74984737632e4b421f5056e62a290f57409ab8e
Ray Guo <siruiguo@outlook.com>
2020-11-14 22:37:24 +0000

    keep debuging

--- course/management/commands/keep_awake.py
+++ course/management/commands/keep_awake.py
@@ -8,27 +8,25 @@ class Command(BaseCommand):
 
     # define logic of command
     def handle(self, *args, **options):
-        # op = webdriver.ChromeOptions()
-        # op.add_argument("--headless")
-        # # see if the script is being run on cloud or local
-        # if os.environ.get("GOOGLE_CHROME_BIN") is None:
-        #     # on local
-        #     print('Go local.')
-        #     driver = webdriver.Chrome(chrome_options=op) 
-        # else:
-        #     # on cloud
-        #     print('Go cloud.')
-        #     op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        #     op.add_argument("--no-sand box")  # required by heroku
-        #     op.add_argument("--disable-dev-sh-usage")            
-        #     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
         op = webdriver.ChromeOptions()
-        op.add_argument("--headless")  # set headless chrome
-        op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        op.add_argument("--no-sandbox")  # required by heroku
-        op.add_argument("--disable-dev-sh-usage")        
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op) # on cloud
+        op.add_argument("--headless")
+        # see if the script is being run on cloud or local
+        if os.environ.get("GOOGLE_CHROME_BIN") is None:
+            # on local
+            print('Go local.')
+            driver = webdriver.Chrome(chrome_options=op) 
+        else:
+            # on cloud
+            print('Go cloud.')
+            op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            op.add_argument("--no-sandbox")  # required by heroku
+            op.add_argument("--disable-dev-sh-usage")        
+            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op) # on cloud
         
+        # op = webdriver.ChromeOptions()
+        # op.add_argument("--headless")  # set headless chrome
+        
+
         driver.get("https://course-monitor.herokuapp.com/")
         print("Pin the website.")
         time.sleep(1800) # 30 minutes
